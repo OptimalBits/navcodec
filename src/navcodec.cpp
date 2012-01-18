f82fafe408f7f5706eebe2fda63e5a6ad553f666
Manuel Astudillo <manuel@optimalbits.com>
2012-01-18 23:19:24 +0100

    Updated navcodec to use the new modules.

--- src/navcodec.cpp
+++ src/navcodec.cpp
@@ -13,11 +13,29 @@ using namespace v8;
 
 extern "C" { // Cause of name mangling in C++, we use extern C here
   static void init(Handle<Object> target) {
+  
+    // Global initiallization of libavcodec.
+    av_register_all();
+    avformat_network_init();
+    
+    Handle<Array> version = Array::New(3);
+    version->Set(0, Integer::New(LIBAVFORMAT_VERSION_MAJOR));
+    version->Set(1, Integer::New(LIBAVFORMAT_VERSION_MINOR));
+    version->Set(2, Integer::New(LIBAVFORMAT_VERSION_MICRO));
+    
+    target->Set(String::NewSymbol("AVFormatVersion"), version); 
+    
+    target->Set(String::NewSymbol("PixelFormat"), CreatePixelFormatsEnum()); 
+    
     AVFormat::Init(target);
-    _AVFrame::Init(target);
+    NAVOutputFormat::Init(target);
+    NAVSws::Init(target);
     
+    // Objects only instantiable from C++
+    _AVFrame::Init();
+    _AVStream::Init();    
     _AVCodecContext::Init();
-    
   }
   NODE_MODULE(navcodec, init);
 }
+
--- src/navcodec.h
+++ src/navcodec.h
@@ -1,6 +1,8 @@
 
 #include "avformat.h"
+#include "navoutformat.h"
 #include "avcodeccontext.h"
 #include "avframe.h"
 #include "avstream.h"
-
+#include "navpixformat.h"
+#include "navsws.h"
